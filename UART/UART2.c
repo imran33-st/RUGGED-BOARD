72eec21e16b279e828668db63f93edcd0b06dd2b
imran33-st <amdimran3105@gmail.com>
2026-09-25 13:59:53 +0530

    Enhance UART loopback to read and display strings
    Modified the UART loopback program to handle string input and display received strings. Improved comments for clarity.

--- UART/UART2.c
+++ UART/UART2.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 #include <mraa/uart.h>
 
 int main()
@@ -7,8 +8,9 @@ int main()
     mraa_uart_context uart;
     char tx_data;
     char rx_data;
+    char rx_buffer[100];
+    int index = 0;
 
-    /* Initialize UART3 */
     uart = mraa_uart_init(3);
 
     if (uart == NULL)
@@ -17,37 +19,47 @@ int main()
         return 1;
     }
 
-    /* Set UART parameters */
     mraa_uart_set_baudrate(uart, 115200);
     mraa_uart_set_mode(uart, 8, MRAA_UART_PARITY_NONE, 1);
 
     printf("UART loopback started on /dev/ttyS3\n");
-    printf("TX and RX must be connected together.\n");
-    printf("Type something:\n");
+    printf("Enter a word and press Enter:\n");
 
     while (1)
     {
-        /* Get keyboard input */
+        /* Get keyboard character */
         tx_data = getchar();
 
-        /* Send data through UART */
+        /* Send character through UART */
         mraa_uart_write(uart, &tx_data, 1);
 
-        /* Wait until data is received */
+        /* Wait for received character */
         while (mraa_uart_data_available(uart, 1000) == 0)
         {
-            /* Wait */
         }
 
-        /* Read received data */
+        /* Read received character */
         mraa_uart_read(uart, &rx_data, 1);
 
-        /* Display received data */
-        printf("Received: %c\n", rx_data);
-        fflush(stdout);
+        /* Store received character */
+        if (rx_data == '\n')
+        {
+            rx_buffer[index] = '\0';
+
+            printf("Received: %s\n", rx_buffer);
+
+            index = 0;
+        }
+        else if (rx_data != '\r')
+        {
+            if (index < 99)
+            {
+                rx_buffer[index] = rx_data;
+                index++;
+            }
+        }
     }
 
-    /* Close UART */
     mraa_uart_stop(uart);
 
     return 0;
